493d769d5a2c1d1fcb057ca4fd87f0093cf19778
henilMistry <henilmistry74496@gmail.com>
2023-12-07 23:09:56 +0530

    Added some comments to help me later, good dev. steps

--- src/main/Dialogs/Dialog_Device.java
+++ src/main/Dialogs/Dialog_Device.java
@@ -3,6 +3,15 @@ package Dialogs;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * Program name: Dialog_Device
+ * Program date: 13th November 2023
+ * Program owner: Henil Mistry
+ * Contributor:
+ * Last Modified: 07th December 2023
+ *
+ * Purpose: This class helps for setting up the 'Device' (in terms of NS3)
+ * */
 public class Dialog_Device extends JFrame {
     private JPanel JPanel_main;
     private JLabel lbl_info;
@@ -25,7 +34,11 @@ public class Dialog_Device extends JFrame {
         this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
     }
 
+    /*
+    // Completed on : 07th Dec. 2023
+    // This is the example of Constructor for this dialog...
     public static void main(String[] args) {
         new Dialog_Device("Device configuration for node 0 and 1");
     }
+    */
 }
