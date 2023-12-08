195bf7cf73e056d47111d55c883cb426c8dd6c3f
henilMistry <henilmistry74496@gmail.com>
2023-12-09 00:58:55 +0530

    Major change in both of these, changes in constructors of both, added plenty of functions and added window closing event listener for confirmation, added events on button click, collected information about devices in list through these two

--- src/main/Dialogs/Dialog_Device.java
+++ src/main/Dialogs/Dialog_Device.java
@@ -1,14 +1,22 @@
 package Dialogs;
 
+import Helpers.DeviceHelper;
+import Helpers.LinkHelper;
+import Helpers.NetworkHelper;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 /**
  * Program name: Dialog_Device
  * Program date: 13th November 2023
  * Program owner: Henil Mistry
  * Contributor:
- * Last Modified: 07th December 2023
+ * Last Modified: 08th December 2023
  *
  * Purpose: This class helps for setting up the 'Device' (in terms of NS3)
  * */
@@ -22,23 +30,76 @@ public class Dialog_Device extends JFrame {
     private JComboBox comboBox_net_config;
     private JButton btn_configDevice;
 
-    public Dialog_Device(String LblDevice) {
+    // Functionalities Required Below Things...
+    ArrayList<LinkHelper> links;
+    ArrayList<NetworkHelper> networks;
+    Dialog_Topology parent;
+    String nodeA,nodeB;
+
+    // change : Changed the constructor for not compromising the functionalities served by this Dialog...
+    // changed made on : 08th Dec., 2023
+    public Dialog_Device(String a, String b, String LblDevice, ArrayList<LinkHelper> l, ArrayList<NetworkHelper> n, Dialog_Topology parent) {
         // initializing this component...
         this.lbl_info.setText(LblDevice);
+        this.nodeA = a;
+        this.nodeB = b;
 
+        // init. the lists and other things...
+        this.links = new ArrayList<>();
+        this.links.addAll(l);
+        this.networks = new ArrayList<>();
+        this.networks.addAll(n);
+        this.parent = parent;
+
+        // init. the combo boxes...
+        for (LinkHelper link : this.links) {
+            this.comboBox_link_config.addItem(link);
+        }
+        for (NetworkHelper network : this.networks) {
+            this.comboBox_net_config.addItem(network);
+        }
+
+        // initializing the panel and making it visible...
         this.setContentPane(this.JPanel_main);
         this.setTitle("Configure Device");
         this.setSize(400,200);
         this.setVisible(true);
         this.setResizable(false);
         this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        btn_configDevice.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                addDeviceToParent();
+            }
+        });
+    }
+
+    // added : Added this function for adding the Device in Device_Topology (aka parent of this class's object)
+    // added on : 08th Dec., 2023
+    private void addDeviceToParent() {
+        String nodeGrp = this.nodeA + this.nodeB;
+        boolean collision = false;
+
+        for (int i=0; i<this.parent.devices.size(); i++) {
+            if(this.parent.devices.get(i).nodesGroup.compareToIgnoreCase(nodeGrp) == 0) {
+                this.parent.devices.add(new DeviceHelper(this.links.get(this.comboBox_link_config.getSelectedIndex()),this.networks.get(this.comboBox_net_config.getSelectedIndex()), nodeGrp));
+                this.parent.devices.remove(i);
+                collision = true;
+            }
+        }
+
+        if (!collision) {
+            this.parent.devices.add(new DeviceHelper(this.links.get(this.comboBox_link_config.getSelectedIndex()),this.networks.get(this.comboBox_net_config.getSelectedIndex()), nodeGrp));
+        }
+        JOptionPane.showMessageDialog(this,"Device has been configured!", "Device Config.", JOptionPane.INFORMATION_MESSAGE);
+        this.setVisible(false);
     }
 
     /*
     // Completed on : 07th Dec. 2023
     // This is the example of Constructor for this dialog...
     public static void main(String[] args) {
-        new Dialog_Device("Device configuration for node 0 and 1");
+        // new Dialog_Device(0,1,"Device configuration for node 0 and 1",<links>,<networks>);
     }
     */
 }
--- src/main/Dialogs/Dialog_Topology.java
+++ src/main/Dialogs/Dialog_Topology.java
@@ -1,8 +1,27 @@
 package Dialogs;
 
+import Helpers.DeviceHelper;
+import Helpers.LinkHelper;
+import Helpers.NetworkHelper;
+
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 
+
+/**
+ * Program name: Dialog_Topology
+ * Program date: 12th November 2023
+ * Program owner: Henil Mistry
+ * Contributor:
+ * Last Modified: 08th December 2023
+ *
+ * Purpose: This class helps for setting up the 'Topology' in GUI form.
+ * */
 public class Dialog_Topology extends JFrame {
     private JPanel JPanel_main;
     private JPanel JPanel_topology_btns;
@@ -13,19 +32,41 @@ public class Dialog_Topology extends JFrame {
     // for image icon to be used on button...
     ImageIcon icon = new ImageIcon("src/main/Resources/link_color.png");
     Image img = icon.getImage();
+    // for storing the devices, and it's configuration...
+    ArrayList<DeviceHelper> devices;
+    int grid;
+    int nodes;
+    // for passing it to the Dialog_Device...
+    ArrayList<LinkHelper> links;
+    ArrayList<NetworkHelper> networks;
 
-    public Dialog_Topology(int gridSize) {
+    public Dialog_Topology(int gridSize, ArrayList<LinkHelper> l, ArrayList<NetworkHelper> n) {
+
+        // initializing the components...
+        this.devices = new ArrayList<>();
+        this.links = l;
+        this.networks = n;
 
         // for grid calculation...
-        int grid = (int) Math.floor(gridSize/2);
+        this.nodes = gridSize;
+        this.grid = (int) Math.floor(this.nodes/2);
 
         this.setContentPane(this.JPanel_main);
         this.JPanel_topology_btns.setLayout(new GridLayout(grid,grid*2,3,3));
 
+        // change : added the action listener on each button which is added...
+        // change made on : 08th Dec., 2023
         // for generating button...
         for(int i=0; i<gridSize; i++) {
             JButton btn = new JButton(String.valueOf(i));
             btn.setHorizontalTextPosition(SwingConstants.TRAILING);
+            btn.setActionCommand(String.valueOf(i));
+            btn.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    openDevicesDialog(Integer.parseInt(e.getActionCommand()));
+                }
+            });
             this.JPanel_topology_btns.add(btn);
             Image scaledImg = img.getScaledInstance(100,100,Image.SCALE_SMOOTH);
             ImageIcon linkIcon = new ImageIcon(scaledImg);
@@ -37,7 +78,55 @@ public class Dialog_Topology extends JFrame {
         this.setSize(500,500);
         this.setVisible(true);
         this.setResizable(false);
-        this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+        this.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        btn_setUp.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                printDeviceConfig();
+                showInfoMsg();
+            }
+        });
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e);
+
+                AskForConfirmation();
+            }
+        });
+    }
+
+    private void AskForConfirmation() {
+        // System.out.println("Nodes : "+this.nodes+" Devices : "+this.devices.size());
+        if (this.nodes != this.devices.size()) {
+            int option = JOptionPane.showConfirmDialog(JPanel_main,"Some or All devices are left configured!\nAre you sure you want to close the topology config?","Quit!",JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE);
+            if(option == JOptionPane.YES_OPTION) {
+                this.setVisible(false);
+            }
+        } else {
+            this.setVisible(false);
+        }
+    }
+
+    private void showInfoMsg() {
+        if (this.nodes == this.devices.size()) {
+            JOptionPane.showMessageDialog(this,"Topology has been configured successfully!", "Topology Config.", JOptionPane.INFORMATION_MESSAGE);
+            this.setVisible(false);
+        } else {
+            AskForConfirmation();
+        }
+    }
+
+    private void printDeviceConfig() {
+        for (DeviceHelper device : this.devices) {
+            System.out.println(device.getDeviceConfCode());
+        }
+    }
+
+    // added : Added this function because 'i' cannot be accessed outside of scope...
+    private void openDevicesDialog(int i) {
+        // System.out.println("GRID : "+this.nodes+" i : "+i);
+        Dialog_Device dialogDevice = new Dialog_Device(String.valueOf(i),String.valueOf(((i+1)%this.nodes)), "Device configuration for node "+i+" and "+((i+1)%this.nodes),this.links, this.networks, this);
     }
 
     /*
