bb0b5ff5b031c6d541fc7c0c2d1ed6e7f01062c1
henilMistry <henilmistry74496@gmail.com>
2024-02-19 12:30:21 +0530

    Added a static method for checking the type of link and added some variable with getter and setter for EnablePcap property

--- src/main/Links/NetworkLink.java
+++ src/main/Links/NetworkLink.java
@@ -3,7 +3,13 @@ package Links;
 import StatusHelper.LinkType;
 
 public interface NetworkLink {
-    
+
+    int CHOICE_INDEX_P2P = 0;
+    int CHOICE_INDEX_CSMA = 1;
+
+    String LABEL_P2P = "P2P";
+    String LABEL_CSMA = "CSMA";
+
     public int getId();
 
     public void setId(int id);
@@ -28,6 +34,10 @@ public interface NetworkLink {
 
     public void setLinkType(LinkType linkType);
 
+    public boolean getEnablePcap();
+
+    public void setEnablePcap(boolean enablePcap);
+
     // for storing in file for settings...
     public String forSettings();
 
@@ -39,4 +49,13 @@ public interface NetworkLink {
      * */
     public String toCode();
 
+    static LinkType getLinkType(int choiceIndex) {
+        if(choiceIndex == CHOICE_INDEX_P2P) {
+            return LinkType.LINK_P2P;
+        } else if (choiceIndex == CHOICE_INDEX_CSMA){
+            return LinkType.LINK_CSMA;
+        } else { // default case...
+            return LinkType.LINK_P2P;
+        }
+    }
 }
