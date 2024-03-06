2fe97a86d0c298bcc14bcfb24d49a58982d3cd0e
HenilRMistry <henilmistry74496@gmail.com>
2024-03-06 13:57:12 +0530

    Added icons for painting of wi-fi links

---
+++ src/main/Ns3Objects/Links/WIFI.java
@@ -0,0 +1,12 @@
+/**
+ * Program name: WIFI
+ * Program date: 05-03-2024
+ * Program owner: henil
+ * Contributor: 
+ * Last Modified: 05-03-2024
+ *
+ * Purpose: 
+ * */
+package Ns3Objects.Links;
+public class WIFI {
+}
