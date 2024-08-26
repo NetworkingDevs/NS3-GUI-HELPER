0328168ddd60e15a54a5b12a062024d0b1887088
HenilMistry <henilmistry74496@gmail.com>
2024-08-26 10:39:04 +0530

    Added a Design for UdpEchoServer DalogBox

---
+++ src/main/Dialogs/Dialog_UdpEchoServer.java
@@ -0,0 +1,19 @@
+package Dialogs;
+
+import javax.swing.*;
+
+public class Dialog_UdpEchoServer extends JFrame implements Dialog {
+    private JPanel JPanel_main;
+    private JLabel lbl_serverIndex;
+    private JLabel lbl_selectedServerIndex;
+    private JTextField textField_portNo;
+    private JLabel lbl_portNo;
+    private JLabel lbl_startTime;
+    private JTextField textField_startTime;
+    private JLabel lbl_upTime;
+    private JTextField textField_upTime;
+    private JButton btn_Save;
+
+
+
+}
