96d604dfe5118f7edb03d064bc1dc9ec730fc8af
HenilMistry <henilmistry74496@gmail.com>
2024-08-26 10:39:36 +0530

    Added a new interface for Dialog

---
+++ src/main/Dialogs/Dialog.java
@@ -0,0 +1,68 @@
+package Dialogs;
+
+public interface Dialog {
+    /**
+     * placeholder for start time
+     * */
+    String PLACEHOLDER_START_TIME = "Enter start time";
+    /**
+     * placeholder for uptime
+     * */
+    String PLACEHOLDER_UP_TIME = "Enter total up time";
+    /**
+     * placeholder for MTU
+     * */
+    String PLACEHOLDER_MTU = "Enter the MTU";
+    /**
+     * placeholder for interval between two packets
+     * */
+    String PLACEHOLDER_INTERVAL = "Enter the interval";
+    /**
+     * placeholder for total packets to be sent
+     * */
+    String PLACEHOLDER_TOT_PACKETS = "Enter total packets";
+    /**
+     * placeholder for port number
+     * */
+    String PLACEHOLDER_PORT = "Enter the port number";
+    /**
+     * placeholder for delay
+     * */
+    String PLACEHOLDER_DELAY = "Enter delay (in ms)";
+    /**
+     * placeholder for data rate
+     * */
+    String PLACEHOLDER_DATA_RATE = "Enter data rate";
+    /**
+     * placeholder for alias name
+     * */
+    String PLACEHOLDER_ALIAS_NAME = "Enter alias name";
+    /**
+     * placeholder for net id
+     * */
+    String PLACEHOLDER_NET_ID = "Enter NetID";
+    /**
+     * placeholder for subnet mask
+     * */
+    String PLACEHOLDER_SUBNET_MASK = "Enter netmask";
+    /**
+     * placeholder for ssid
+     * */
+    String PLACEHOLDER_SSID = "Enter SSID";
+    /**
+     * The placeholder for output path
+     * */
+    String PLACEHOLDER_OUTPUT_PATH = "Enter path for output path";
+    /**
+     * The placeholder for file name
+     * */
+    String PLACEHOLDER_FILE_NAME = "Enter name for output file";
+    /**
+     * the key for the combo box
+     * */
+    String COMPONENT_COMBO_BOX = "WiFiLink_ComboBox";
+    /**
+     * the key for the overview label
+     * */
+    String COMPONENT_OVERVIEW_LABEL = "WiFiLink_OverviewLabel";
+}
