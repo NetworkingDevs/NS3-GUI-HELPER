3040ad49049f7de31cc0129774382452291a2d1d
HenilMistry <henilmistry74496@gmail.com>
2024-08-28 11:33:11 +0530

    Added a New enum for new UdpEchoServerTool

--- src/main/StatusHelper/ToolStatus.java
+++ src/main/StatusHelper/ToolStatus.java
@@ -4,5 +4,5 @@ package StatusHelper;
  * enum type for identification of tool
  * */
 public enum ToolStatus {
-    TOOL_NODE, TOOL_LINK, TOOL_VIEW, TOOL_LINK_CSMA, TOOL_LINK_WIFI
+    TOOL_NODE, TOOL_LINK, TOOL_VIEW, TOOL_LINK_CSMA, TOOL_LINK_WIFI, TOOL_CONFIG_ECHO_SERVER
 }
